60bb31ba41693e6016267f05a66e1e9e64e7bbc4
Sewak <legendyaar10@gmail.com>
2025-04-25 00:23:53 -0700

    fix port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
