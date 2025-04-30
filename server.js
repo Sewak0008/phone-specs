4a2d6212ba6295aa91b120ff6b565af2967355bd
Sewak <legendyaar10@gmail.com>
2025-04-30 04:13:43 -0700

    repalce code

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
+// server.js
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const movieRoutes = require('./routes/movieRoutes');
 
 const app = express();
+app.use(express.static(__dirname));
 const PORT = process.env.PORT || 3000;
 
-// MongoDB Connect
-mongoose.connect("mongodb://localhost:27017/moviesite", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routes
-app.use('/', movieRoutes);
+const dataFile = './movies.json';
 
-// Start server
+// Load movies from JSON file
+function loadMovies() {
+  if (!fs.existsSync(dataFile)) return [];
+  return JSON.parse(fs.readFileSync(dataFile));
+}
+
+// Save movies to JSON file
+function saveMovies(movies) {
+  fs.writeFileSync(dataFile, JSON.stringify(movies, null, 2));
+}
+
+app.get('/', (req, res) => {
+  const movies = loadMovies();
+  const categories = {};
+  movies.forEach(movie => {
+    if (!categories[movie.category]) categories[movie.category] = [];
+    categories[movie.category].push(movie);
+  });
+  res.render('index', { categories });
+});
+
+app.get('/add', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/add.html'));
+});
+
+app.post('/add', (req, res) => {
+  const { title, link, category, thumbnail } = req.body;
+  const movies = loadMovies();
+  movies.push({ title, link, category, thumbnail });
+  saveMovies(movies);
+  res.redirect('/');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
